750ae9ff200c178f918812f0fc2a614b0f2b83d4
Shubham Narula <shubham.narula@antiersolutions.com>
2022-01-07 12:53:06 +0530

    working on home screen

--- app/src/main/java/com/xxmukulxx/notes/feature_home/presentation/HomeViewModel.kt
+++ app/src/main/java/com/xxmukulxx/notes/feature_home/presentation/HomeViewModel.kt
@@ -1,9 +1,12 @@
 package com.xxmukulxx.notes.feature_home.presentation
 
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.xxmukulxx.notes.R
 import com.xxmukulxx.notes.common.BaseViewModel
 import com.xxmukulxx.notes.feature_main.presentation.MainFragment
-import com.xxmukulxx.notes.util.getString
+import com.xxmukulxx.notes.util.toast
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -11,7 +14,26 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor() : BaseViewModel() {
     lateinit var mainFragment: MainFragment
 
+    private val searchBar: MutableLiveData<Boolean> =
+        MutableLiveData(false)
+    fun isSearchBarVisible (): LiveData<Boolean> {
+        return searchBar
+    }
+
+    fun setSearchBarVisibility(value:Boolean){
+        searchBar.postValue(value)
+    }
+
     fun setAppBar() {
         mainFragment.setAppBar("")
     }
+
+    fun handleClick(view:View){
+        when(view.id){
+            R.id.iv_search ->{
+                setSearchBarVisibility(!searchBar.value!!)
+            }
+        }
+    }
+
 }
