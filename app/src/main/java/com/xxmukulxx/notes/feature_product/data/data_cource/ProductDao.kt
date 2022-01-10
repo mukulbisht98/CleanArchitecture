59c7f6064aeecccf2f08fcfea2bb8b4e8b8d74d8
Shubham Narula <shubham.narula@antiersolutions.com>
2022-01-10 11:10:17 +0530

    add and change product db queries

--- app/src/main/java/com/xxmukulxx/notes/feature_product/data/data_cource/ProductDao.kt
+++ app/src/main/java/com/xxmukulxx/notes/feature_product/data/data_cource/ProductDao.kt
@@ -6,14 +6,21 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ProductDao {
-    @Query("SELECT * FROM productdata")
+    @Query("SELECT * FROM Productdata ORDER BY title ASC")
     fun getProducts(): Flow<List<ProductData>>
 
     @Query("SELECT * FROM Productdata WHERE id = :id")
     suspend fun getProductById(id: Int): ProductData?
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert
     suspend fun insertProduct(product: ProductData)
+
+
+    @Query("Update Productdata set title =:title,description=:description,type =:type,price =:price where id=:productId")
+    suspend fun updateProduct(title:String,description:Int,type:String,price:String,productId:Int)
+
+    @Update
+    suspend fun update(productData: ProductData)
 
     @Delete
     suspend fun deleteProduct(product: ProductData)
