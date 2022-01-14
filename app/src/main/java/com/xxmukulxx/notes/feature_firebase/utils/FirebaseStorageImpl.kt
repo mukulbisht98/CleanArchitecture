40e096db8e35ae74fd4dde9e1b961f2ee03cd3e1
mukul.bisht@antiersolutions.com <mukul.bisht@antiersolutions.com>
2022-01-14 11:36:41 +0530

    added firebase storage class

---
+++ app/src/main/java/com/xxmukulxx/notes/feature_firebase/utils/FirebaseStorageImpl.kt
@@ -0,0 +1,26 @@
+package com.xxmukulxx.notes.feature_firebase.utils
+
+import android.net.Uri
+import android.util.Log
+import com.google.firebase.storage.FirebaseStorage
+import com.xxmukulxx.notes.util.toast
+import java.util.*
+
+class FirebaseStorageImpl {
+    companion object {
+        const val TAG = "FIREBASE_STORAGE ->"
+    }
+
+    var firebaseStorage = FirebaseStorage.getInstance()
+    var storageRef = firebaseStorage.reference.child("images/" + UUID.randomUUID())
+
+
+    fun uploadImageToFirebase(uri: Uri) {
+        storageRef.putFile(uri).addOnSuccessListener {
+            toast("Successfully uploaded!")
+        }.addOnFailureListener {
+            Log.e(TAG, "uploadImageToFirebase: ${it.message}")
+        }.addOnProgressListener {
+        }
+    }
+}
